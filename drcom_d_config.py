1050e180f812876e45178f3bfda06aafad3e782d
mchome <fnatic.roach@gmail.com>
2015-04-02 11:01:54 +0000

    change output

--- drcom_d_config.py
+++ drcom_d_config.py
@@ -1,10 +1,4 @@
 # -*- coding: utf-8 -*-
-"""
-Created on Wed Dec 03 23:45:49 2014
-
-@author: latyas
-"""
-
 from binascii import hexlify
 import re
 
@@ -19,25 +13,28 @@ f = open(filename, 'rb')
 text = f.read()
 offset = re.search('\xf0\x00\xf0\x00[\x00-\xFF]{4}\x03\x01', text).start() + 8
 #print hexlify(text[offset:offset+330])
-print 'pcapng file:', filename
-print 'copy following statements to drcom.conf or overwrite field between "# CONFIG" and "# CONFIG_END" in latest-wired.py'
-print '\n'
 username_len = ord(text[offset+3]) - 20
 username = text[offset+20:offset+20+username_len]
-print 'server = \'%s\'' % '.'.join([str(ord(i)) for i in text[offset-12:offset-8]])
-print 'username=\'%s\'' % username
-print 'password=\'\''
-print 'CONTROLCHECKSTATUS = \'%s\'' % hexed(text[offset+56])
-print 'ADAPTERNUM = \'%s\'' % hexed(text[offset+57])
-print 'host_ip = \'%s\'' % '.'.join(map(lambda x: str(ord(x)), text[offset+81:offset+85]))
-print 'IPDOG = \'%s\'' % hexed(text[offset+105])
-print 'host_name = \'%s\'' % 'DRCOMFUCKER'
-print 'PRIMARY_DNS = \'%s\'' % '.'.join(map(lambda x: str(ord(x)), text[offset+142 :offset+146]))
-print 'dhcp_server = \'%s\'' % '.'.join(map(lambda x: str(ord(x)), text[offset+146:offset+150]))
-print 'AUTH_VERSION = \'%s\'' % hexed(text[offset+310:offset+312])
-print 'mac = 0x%s' % hexlify(text[offset+320:offset+326])
-print 'host_os = \'%s\'' % 'WINDIAOS'
+passwd = raw_input('Please input your password!\n')
 
-# now get the version field in heartbeat
+server =  'server = \'%s\'' % '.'.join([str(ord(i)) for i in text[offset-12:offset-8]])
+username = 'username = \'%s\'' % username
+password = 'password = \'' + passwd + '\''
+CONTROLCHECKSTATUS = 'CONTROLCHECKSTATUS = \'%s\'' % hexed(text[offset+56])
+ADAPTERNUM = 'ADAPTERNUM = \'%s\'' % hexed(text[offset+57])
+host_ip = 'host_ip = \'%s\'' % '.'.join(map(lambda x: str(ord(x)), text[offset+81:offset+85]))
+IPDOG = 'IPDOG = \'%s\'' % hexed(text[offset+105])
+host_name = 'host_name = \'%s\'' % 'DRCOMFUCKER'
+PRIMARY_DNS = 'PRIMARY_DNS = \'%s\'' % '.'.join(map(lambda x: str(ord(x)), text[offset+142 :offset+146]))
+dhcp_server = 'dhcp_server = \'%s\'' % '.'.join(map(lambda x: str(ord(x)), text[offset+146:offset+150]))
+AUTH_VERSION = 'AUTH_VERSION = \'%s\'' % hexed(text[offset+310:offset+312])
+mac = 'mac = 0x%s' % hexlify(text[offset+320:offset+326])
+host_os = 'host_os = \'%s\'' % 'WINDIAOS'
 KEEP_ALIVE_VERSION = re.search('\xf0\x00\xf0\x00....\x07.\x5c\x28\x00\x0b\x01(..)', text).group(1)
-print 'KEEP_ALIVE_VERSION = \'%s\'' % hexed(KEEP_ALIVE_VERSION)
+KEEP_ALIVE_VERSION = 'KEEP_ALIVE_VERSION = \'%s\'' % hexed(KEEP_ALIVE_VERSION)
+
+content = open('config.conf', 'w')
+for args in [server,username,password,CONTROLCHECKSTATUS,ADAPTERNUM,host_ip,IPDOG,host_name,PRIMARY_DNS,dhcp_server,AUTH_VERSION,mac,host_os,KEEP_ALIVE_VERSION]:
+    print args
+    content.write(args+'\n')
+content.close()
